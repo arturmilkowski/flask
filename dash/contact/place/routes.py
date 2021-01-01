b61990d0745b825277812c74158e5f379a826340
Artur Miłkowski <artur-milkowski@tlen.pl>
2021-01-01 16:18:34 +0100

    Add template folders

--- dash/contact/place/routes.py
+++ dash/contact/place/routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-place = Blueprint('place', __name__, url_prefix='/dash/contacts/places')
+place = Blueprint(
+    'place',
+    __name__,
+    template_folder="templates",
+    url_prefix='/dash/contacts/places'
+)
 
 @place.route('/')
 def index():
-    return "dash.place.index"
+    # return "dash.place.index"
+    return render_template('index.html')
+
--- dash/contact/routes.py
+++ dash/contact/routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-contact = Blueprint('contact', __name__, url_prefix='/dash/contacts')
+contact = Blueprint(
+    'contact',
+    __name__,
+    template_folder="templates",
+    url_prefix='/dash/contacts'
+)
 
 @contact.route('/')
 def index():
-    return "dash.contact.index"
+    # return "dash.contact.index"
+    return render_template('index.html')
