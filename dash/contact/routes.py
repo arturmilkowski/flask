5b0704285d11943b3111d8fd5891ec8c2912e3fb
Artur Miłkowski <artur-milkowski@tlen.pl>
2021-01-01 17:05:52 +0100

    Add rest api style routes to contact

--- dash/contact/routes.py
+++ dash/contact/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+# from typing import str
+# from typing import Integer
+from flask import Blueprint, render_template  # , Response
 
 contact = Blueprint(
     'contact',
@@ -8,6 +10,30 @@ contact = Blueprint(
 )
 
 @contact.route('/')
-def index():
+def index() -> str:
     # return "dash.contact.index"
     return render_template('index.html')
+
+@contact.route('/create')
+def create() -> str:
+    return "dash.contact.create"
+
+@contact.route('/', methods=['POST'])
+def store() -> str:
+    return "dash.contact.store"
+
+@contact.route('/<int:id>')
+def show(id: int) -> str:
+    return f"dash.contact.show {id}"
+
+@contact.route('/<int:id>/edit')
+def edit(id: int) -> str:
+    return f"dash.contact.edit {id}"
+
+@contact.route('/<int:id>', methods=['PUT'])
+def update(id: int) -> str:
+    return f"dash.contact.update {id}"
+
+@contact.route('/<int:id>', methods=['DELETE'])
+def destroy(id: int) -> str:
+    return f"dash.contact.destroy {id}"
